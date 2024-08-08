b3ca3703255637f80e68783c674705760ce2967d
Igor Ramos <i.ramos@aluno.ifsp.edu.br>
2024-08-08 20:13:02 -0300

    .

--- hello.py
+++ hello.py
@@ -12,8 +12,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
@@ -23,76 +23,76 @@ migrate = Migrate(app, db)
 
 # Modelos de banco de dados
 class Role(db.Model):
-__tablename__ = 'roles'
-id = db.Column(db.Integer, primary_key=True)
-name = db.Column(db.String(64), unique=True)
-users = db.relationship('User', backref='role', lazy='dynamic')
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
-def __repr__(self):
-return '' % self.name
+    def __repr__(self):
+        return f'<Role {self.name}>'
 
 
 class User(db.Model):
-__tablename__ = 'users'
-id = db.Column(db.Integer, primary_key=True)
-username = db.Column(db.String(64), unique=True, index=True)
-role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-def __repr__(self):
-return '' % self.username
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 
 # Formulário com campos para nome e seleção de função
 class NameForm(FlaskForm):
-name = StringField('What is your name?', validators=[DataRequired()])
-role = SelectField('Role?', choices=[], validators=[DataRequired()])
-submit = SubmitField('Submit')
+    name = StringField('What is your name?', validators=[DataRequired()])
+    role = SelectField('Role?', choices=[], validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.shell_context_processor
 def make_shell_context():
-return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-return render_template('404.html'), 404
+    return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-return render_template('500.html'), 500
+    return render_template('500.html'), 500
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-form = NameForm()
+    form = NameForm()
 
-# Preenche as opções do campo de função dinamicamente
-form.role.choices = [(role.id, role.name) for role in Role.query.order_by('name')]
+    # Preenche as opções do campo de função dinamicamente
+    form.role.choices = [(role.id, role.name) for role in Role.query.order_by('name')]
 
-if form.validate_on_submit():
-role = Role.query.filter_by(id=form.role.data).first()
-user = User.query.filter_by(username=form.name.data).first()
+    if form.validate_on_submit():
+        role = Role.query.filter_by(id=form.role.data).first()
+        user = User.query.filter_by(username=form.name.data).first()
 
-if user is None:
-user = User(username=form.name.data, role=role)
-db.session.add(user)
-db.session.commit()
-session['known'] = False
-else:
-session['known'] = True
+        if user is None:
+            user = User(username=form.name.data, role=role)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
 
-session['name'] = form.name.data
-session['role'] = role.name
-return redirect(url_for('index'))
+        session['name'] = form.name.data
+        session['role'] = role.name
+        return redirect(url_for('index'))
 
-users = User.query.all()
-roles = Role.query.all()
+    users = User.query.all()
+    roles = Role.query.all()
 
-return render_template('index.html', form=form, name=session.get('name'),
-known=session.get('known', False), users=users, roles=roles)
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False), user_all=users, roles=roles)
 
 
 if __name__ == '__main__':
-app.run(debug=True)
+    app.run(debug=True)
